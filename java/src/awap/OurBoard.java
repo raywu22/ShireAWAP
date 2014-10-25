c292c0528218201ffbd74de30fed734ed2ec6e79
Raymond Wu <raywu22@mit.edu>
2014-10-25 18:21:42 -0400

    added update

--- java/src/awap/OurBoard.java
+++ java/src/awap/OurBoard.java
@@ -75,6 +75,7 @@ public class OurBoard {
 		    		scoreOfNewBlock *= 3;
 		    }
 	    }
+	    updateCorners();
 	    return new OurBoard(this.getCorners(),copyOfBlockPlacements, this.score + scoreOfNewBlock, pointsToAdd);
 	}
 	/**
